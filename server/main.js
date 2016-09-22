2ecb5b7ff38fce16d3fc1d31d3e2cf29a07d7f91
Kevin Korte <kevinkorte15@gmail.com>
2016-09-21 20:05:23 -0700

    fixed startup error, loaded on server, not client

--- server/main.js
+++ server/main.js
@@ -2,10 +2,3 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
 });
-
-Meteor.startup(function() {
-  GoogleMaps.load({
-    key: Meteor.settings.public.mapsapi,
-    libraries: 'places'  // also accepts an array if you need more than one
-  });
-});
