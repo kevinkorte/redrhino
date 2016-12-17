20b6c3fec032bd0e376b21d99984c88f5ea66830
Kevin Korte <kevinkorte15@gmail.com>
2016-12-16 17:41:38 -0800

    working login page password reset

--- client/login/loginForm.js
+++ client/login/loginForm.js
@@ -10,5 +10,26 @@ Template.loginForm.events({
         FlowRouter.go('home');
       }
     })
+  },
+  'click .password-btn.pw-unhide'(event) {
+    event.preventDefault();
+    $('#password').prop('type', 'text');
+    $('.password-btn').removeClass('pw-unhide').addClass('pw-hide').children().removeClass('unhide').addClass('hide');
+  },
+  'click .password-btn.pw-hide'(event) {
+    event.preventDefault();
+    $('#password').prop('type', 'password');
+    $('.password-btn').removeClass('pw-hide').addClass('pw-unhide').children().removeClass('hide').addClass('unhide');
+  },
+  'submit .sendPasswordReset'(event) {
+    event.preventDefault();
+    const email = event.target.loginEmail.value;
+    Accounts.forgotPassword({email:email}, function(error) {
+      if (error) {
+        Bert.alert( error.reason, 'danger', 'growl-top-right' );
+      } else {
+        Bert.alert( 'Sweet, check your email', 'success', 'growl-top-right', 'fa-check' );
+      }
+    })
   }
 })
