7a9096e3e6471fc368f8fd165c5d0cc283f91aea
Kevin Korte <kevinkorte15@gmail.com>
2017-01-09 15:13:23 -0800

    added kadira apm

--- server/main.js
+++ server/main.js
@@ -1,8 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
+
 SERVER_AUTH_TOKEN = Random.secret();
 
 Meteor.startup(() => {
   process.env.MAIL_URL = Meteor.settings.private.mailUrl;
-
+  Kadira.connect(Meteor.settings.private.kadira.appId, Meteor.settings.private.kadira.appSecret);
 });
